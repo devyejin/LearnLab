18863ff9de7824410a49c9fe37ca06998a286b8f
yejin <yejinoh58@gmail.com>
2024-11-01 22:16:03 +0900

    feat: fetch todo list

---
+++ app.js
@@ -0,0 +1,62 @@
+// API 베이스 URL
+const URL = 'http://localhost:3000/todos';
+
+// DOMContentLoaded : HTML 문서 로딩이 끝나면 실행되는 이벤트
+// 페이지가 로드되면 Todo 목록 초기화 함수를 실행한다.
+document.addEventListener('DOMContentLoaded', initTodos);
+
+const addTodoButton = document.querySelector('#add-todo');
+// Todo 추가 버튼 클릭 이벤트 리스너
+addTodoButton.addEventListener('click', addTodo);
+
+// 개별 Todo 요소를 화면에 그리는 함수
+function renderTodo(todo) {
+  const todoList = document.querySelector('#todo-list');
+
+  const li = document.createElement('li');
+  li.setAttribute('class', 'todo-item');
+
+  const span = document.createElement('span');
+  span.setAttribute('class', 'todo-text');
+
+  const completeBtn = document.createElement('button');
+  completeBtn.textContent = '완료';
+  completeBtn.setAttribute('class', 'btn btn-primary success-btn');
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = '삭제';
+  deleteBtn.setAttribute('class', 'btn btn-danger delete-btn');
+
+  // const wrapDiv = document.createElement('div');
+  // wrapDiv.setAttribute('class');
+
+  li.append(span, completeBtn, deleteBtn); //append만 한줄로 가능
+
+  todoList.append(li);
+
+  console.log(todoList);
+
+  //데이터 넣어주기
+  span.textContent = todo.content;
+}
+
+// Todo 목록 초기화 (GET 요청)
+async function initTodos() {
+  console.log('Hello World');
+
+  const response = await fetch('http://localhost:3000/todos');
+  const data = await response.json();
+
+  data.forEach((todo) => {
+    renderTodo(todo);
+  });
+}
+
+// Todo 추가하기 (POST 요청)
+async function addTodo() {}
+
+// Todo 완료 상태 토글 (PATCH 요청)
+async function toggleComplete(id) {}
+
+// Todo 삭제하기 (DELETE 요청)
+async function deleteTodo(id) {}
