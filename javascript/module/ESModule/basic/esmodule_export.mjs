1c003a5450ecdfc2737b24593bbfd432eab92ef8
yejin <yejinoh58@gmail.com>
2024-11-03 22:07:03 +0900

    module

---
+++ javascript/module/ESModule/basic/esmodule_export.mjs
@@ -0,0 +1,5 @@
+export const name = '홍길동';
+
+export function greet() {
+  console.log(`안녕, ${name}입니다`);
+}
---
+++ javascript/module/ESModule/default/import_default.js
@@ -0,0 +1,4 @@
+import obj from './export_default.mjs';
+
+console.log(obj.name);
+obj.greet();
