da1195ba5eb82aa1a8a86aa04c9a89c5e568431a
yejin <yejinoh58@gmail.com>
2024-11-07 23:27:59 +0900

    react app 연습 초기 상태

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -1,27 +1,7 @@
-// import React from 'react' <- 러거시라 몰라도 됨
+import React from 'react'
 
-import Profile from './Profile.jsx';
-//import Profile2 from ....
-//이렇게 다른 이름을 쓸 수는 있지만 App명, 파일명, import시 이름도 셋 다 일치시켜주는게 좋다
-//컴포넌트도 하나의 컴포넌트는 하나의 파일로 만드는게 좋음
-
-function Test() {
-  return (
-    <div>
-      <span>ggg</span>
-    </div>
-  );
-}
-
-// 함수앞에 export default 안쓰고 하단에 export default App 해도 됨
 export default function App() {
   return (
-    <div>
-      <div>App</div>
-      <Profile></Profile>
-      <Profile></Profile>
-      <Test></Test>
-      <div>lll</div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
