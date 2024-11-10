68d1893353ff9bf20e99bfb54abea25fde08a0fd
yejin <yejinoh58@gmail.com>
2024-11-10 22:16:34 +0900

    inital state

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -1,25 +1,3 @@
-import React from 'react';
-import NamePage from './NamePage';
-import UserPage from './UserPage';
-import ProductPage from './ProductPage';
-import DogPage from './DogPage';
-import ButtonContainer from './ButtonContainer';
-import MenuPage from './MenuPage';
-import MenuItem from './MenuItem';
-import MenuSection from './MenuSection';
-
-//App에서는 컴포넌트 렌더링만 해주자
 export default function App() {
-  return (
-    <>
-      <NamePage></NamePage>
-      <UserPage></UserPage>
-      <ProductPage></ProductPage>
-      <DogPage></DogPage>
-      <ButtonContainer></ButtonContainer>
-      {/* <MenuItem></MenuItem> */}
-      {/* <MenuSection></MenuSection> */}
-      <MenuPage></MenuPage>
-    </>
-  );
+  return <div>App</div>;
 }
