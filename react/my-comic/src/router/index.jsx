a55e50753ec718c5fe97db77d8ed4b3528f159e1
yejin <yejinoh58@gmail.com>
2024-11-13 14:40:57 +0900

    [etc] router/index.jsx 파일 분리하는 방법 설명

--- react/my-comic/src/router/index.jsx
+++ react/my-comic/src/router/index.jsx
@@ -12,6 +12,21 @@ import MainHome from '../pages/commons/MainHome';
 /**
  * RootLayout이 RootHeader를 가지고 있고, 그 안에 Outlet에서 WebtoonLayout, NovelLayout들을 갖는다.
  */
+
+// 페이지들이 많아지면, children등을 별도의 파일로 분리 가능 -> 유지보수, 관리 측면에서 용이함
+// 분리 예시 <-- 이 변수명를 children에 넣어서 처리 util에 url함수 만들었듯이 분리해서 import 후 사용
+// const webtoonChildren = [[
+//   {
+//     index: true,
+//     element: <WebtoonHome />,
+//   },
+//   {
+//     // 앞에 '/'붙여서 작성하면 절대주소, 안붙이면 상대주소
+//     // /webtoon/:day
+//     path: ':day',
+//     element: <DailyWebtoons />,
+//   },
+// ]]
 const router = createBrowserRouter([
   {
     path: '/',
