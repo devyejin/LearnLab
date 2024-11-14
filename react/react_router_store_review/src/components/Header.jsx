59f1591d3fa1274b80fe14bf8e993ca2a8695b7a
yejin <yejinoh58@gmail.com>
2024-11-15 02:10:56 +0900

    인증 상태에 따른 로그인/로그아웃 버튼 구현 및 상태 변경(store)

--- react/react_router_store_review/src/components/Header.jsx
+++ react/react_router_store_review/src/components/Header.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { login, logout } from '../store/slices/authSlice';
+
+//로그인 상태에 따라 로그인, 로그아웃 버튼을 보여준다.
 
 export default function Header() {
+  //전역 변수(store)에서 상태를 가져온다 useSelector 훅 이용
+  const { isAuthenticated } = useSelector((state) => state.auth); //구조분해할당
+  const dispatch = useDispatch(); //reducer(setter) 사용을 위한 트리거
+
   return (
     <header>
+      <div>
+        {isAuthenticated ? (
+          <button
+            onClick={() => {
+              dispatch(logout());
+            }}
+          >
+            로그아웃
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              dispatch(login());
+            }}
+          >
+            로그인
+          </button>
+        )}
+      </div>
       <ul>
         <li>
           <Link to="/">Home으로</Link>
---
+++ react/react_router_store_review/src/store/slices/authSlice.jsx
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isAuthenticated: true,
+  // isAdmin : false,
+  // user : {
+  //   name : 'anonimoususer',
+  //   id : 'tester@naver.com',
+  // }
+};
+
+const authSlice = createSlice({
+  name: 'auth', //useSelector로 state.auth 이렇게 접근할 때 사용되는 name
+  initialState,
+  //setter들
+  reducers: {
+    login: (state, action) => {
+      state.isAuthenticated = true;
+    },
+    logout: (state, action) => {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+//reducer내보내기
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
