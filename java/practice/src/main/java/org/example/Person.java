2ca58d982c87cb79aab8d6863671637e00b88ae2
yejin <yejinoh58@gmail.com>
2024-12-13 10:50:38 +0900

    인스턴스 메서드와 생성자
    - this 사용 유무
    - 인스턴스 메서드의 경우, 인스턴스 생성 후 실행되기 때문에 this를 명시할 필요가 없다.
    - 메서드의 경우, 매개변수와 인스턴스 변수를 구분짓기 위해 + 인스턴스 생성 전 이기 떄문에 this로 인스턴스 자기자신을 명시한다.

--- java/practice/src/main/java/org/example/Person.java
+++ java/practice/src/main/java/org/example/Person.java
@@ -8,4 +8,17 @@ public class Person {
         this.name = name;
         this.age = age;
     }
+
+    // 오버라이딩
+    // 생성자의 경우, 인스턴스가 생성되기 전에 실행되기 때문에 가리킬 필요가 존재한다.
+    public Person() {
+        this.name = "김나나";
+        this.age = 10;
+    }
+
+    //인스턴스 메서드의 경우, 인스턴스가 생성된 후 실행되기 때문에 this(인스턴스 자기 자신)을 명시할 필요가 없다.
+    public void showPersonInfo() {
+        System.out.printf("name : %s\n age : %d\n", name, age);
+    }
+
 }
